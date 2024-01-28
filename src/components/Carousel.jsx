28a26b9c2c923a81c5de649f129f0bc13e20febf
jobootwori <30703963+jobootwori@users.noreply.github.com>
2024-01-29 00:03:23 +0300

    [UPDATE]: carousel component customization

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import React from "react";
 
 const Carousel = () => {
@@ -12,17 +13,27 @@ const Carousel = () => {
         >
           <div className="carousel-inner">
             <div className="carousel-item active">
-              <img className="w-100" src="img/carousel-1.jpg" alt="Image" />
+              <Image
+                className="w-100"
+                src="/img/carousel-1.jpg"
+                width={0}
+                height={0}
+                sizes="100vw"
+                style={{ width: "100%", height: "auto" }}
+                alt="Image"
+              />
               <div className="carousel-caption">
                 <div className="container">
                   <div className="row justify-content-center">
                     <div className="col-lg-7 pt-5">
-                      <h1 className="display-4 text-white mb-3 animated slideInDown">
-                        Let's Change The World With Humanity
+                      <h1 className="display-5 text-white mb-3 animated slideInDown">
+                        Empathy in Every Encounter Care in Every Moment
                       </h1>
                       <p className="fs-5 text-white-50 mb-5 animated slideInDown">
-                        Aliqu diam amet diam et eos. Clita erat ipsum et lorem
-                        sed stet lorem sit clita duo justo erat amet
+                        Step into a place where compassion meets expertise. The
+                        zambezi Cancer Hospital is honored to stand with you in
+                        your fight against cancer, offering unwavering support
+                        and world-class care
                       </p>
                       <a
                         className="btn btn-primary py-2 px-3 animated slideInDown"
@@ -39,17 +50,28 @@ const Carousel = () => {
               </div>
             </div>
             <div className="carousel-item">
-              <img className="w-100" src="img/carousel-2.jpg" alt="Image" />
+              <Image
+                className="w-100"
+                src="/img/carousel-2.jpg"
+                width={0}
+                height={0}
+                sizes="100vw"
+                style={{ width: "100%", height: "auto" }}
+                alt="Image"
+              />
               <div className="carousel-caption">
                 <div className="container">
                   <div className="row justify-content-center">
                     <div className="col-lg-7 pt-5">
-                      <h1 className="display-4 text-white mb-3 animated slideInDown">
-                        Let's Save More Lifes With Our Helping Hand
+                      <h1 className="display-5 text-white mb-3 animated slideInDown">
+                        Join the BF Suma Zambezi Hospital Cancer Walk
                       </h1>
                       <p className="fs-5 text-white-50 mb-5 animated slideInDown">
-                        Aliqu diam amet diam et eos. Clita erat ipsum et lorem
-                        sed stet lorem sit clita duo justo erat amet
+                        The Cancer Walk is more than a journey; it&apos;s a
+                        collective stride toward hope, support, and a
+                        cancer-free future. Let&apos;s walk together, step by step,
+                        as we celebrate survivors, honor those we&apos;ve lost, and
+                        rally for a brighter tomorrow
                       </p>
                       <a
                         className="btn btn-primary py-2 px-3 animated slideInDown"
@@ -72,7 +94,10 @@ const Carousel = () => {
             data-bs-target="#header-carousel"
             data-bs-slide="prev"
           >
-            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span
+              className="carousel-control-prev-icon"
+              aria-hidden="true"
+            ></span>
             <span className="visually-hidden">Previous</span>
           </button>
           <button
@@ -81,7 +106,10 @@ const Carousel = () => {
             data-bs-target="#header-carousel"
             data-bs-slide="next"
           >
-            <span className="carousel-control-next-icon" aria-hidden="true"></span>
+            <span
+              className="carousel-control-next-icon"
+              aria-hidden="true"
+            ></span>
             <span className="visually-hidden">Next</span>
           </button>
         </div>
