c0cf58c553ace964394c694e13111cdc79ef5e13
jobootwori <30703963+jobootwori@users.noreply.github.com>
2024-01-29 23:01:38 +0300

    [UPDATE]: Changes to href

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -39,7 +39,7 @@ const Header = () => {
             className="navbar navbar-expand-lg navbar-dark py-lg-0 px-lg-5 wow fadeIn"
             data-wow-delay="0.1s"
           >
-            <a href="index.html" className="navbar-brand ms-4 ms-lg-0">
+            <a href="" className="navbar-brand ms-4 ms-lg-0">
               <h1 className="fw-bold text-primary m-0">
                 ZambeziCancer<span className="text-white">Hospital</span>
               </h1>
@@ -54,13 +54,13 @@ const Header = () => {
             </button>
             <div className="collapse navbar-collapse" id="navbarCollapse">
               <div className="navbar-nav ms-auto p-4 p-lg-0">
-                <a href="index.html" className="nav-item nav-link active">
+                <a href="" className="nav-item nav-link active">
                   Home
                 </a>
-                <a href="about.html" className="nav-item nav-link">
+                <a href="" className="nav-item nav-link">
                   About
                 </a>
-                <a href="causes.html" className="nav-item nav-link">
+                <a href="" className="nav-item nav-link">
                   Services
                 </a>
                 <div className="nav-item dropdown">
@@ -72,24 +72,24 @@ const Header = () => {
                     Information Centre
                   </a>
                   <div className="dropdown-menu m-0">
-                    <a href="service.html" className="dropdown-item">
+                    <a href="" className="dropdown-item">
                       Research and Innovation
                     </a>
-                    <a href="donate.html" className="dropdown-item">
+                    <a href="d" className="dropdown-item">
                       Donate
                     </a>
-                    <a href="team.html" className="dropdown-item">
+                    <a href="" className="dropdown-item">
                       Our Team
                     </a>
-                    <a href="testimonial.html" className="dropdown-item">
+                    <a href="" className="dropdown-item">
                       Testimonial
                     </a>
-                    <a href="404.html" className="dropdown-item">
+                    <a href="" className="dropdown-item">
                       Patient Resources
                     </a>
                   </div>
                 </div>
-                <a href="contact.html" className="nav-item nav-link">
+                <a href="" className="nav-item nav-link">
                   Contact
                 </a>
               </div>
