c79f35e854b622b7f84f50857abdc0b0c45855c3
jobootwori <30703963+jobootwori@users.noreply.github.com>
2024-01-30 18:08:20 +0300

    [update] : Contact us Form update

--- src/app/contactus/page.js
+++ src/app/contactus/page.js
@@ -7,7 +7,108 @@ const contactus = () => {
     <>
       <Header />
       <PageHeader />
-      
+
+      {/* <!-- Contact Start --> */}
+      <div className="container-xxl py-5">
+        <div className="container">
+          <div className="row g-5">
+            <div className="col-lg-6 wow fadeIn" data-wow-delay="0.1s">
+              <div className="d-inline-block rounded-pill bg-secondary text-primary py-1 px-3 mb-3">
+                Contact Us
+              </div>
+              <h1 className="display-6 mb-5">
+                If You Have Any Query, Please Contact Us
+              </h1>
+              <p className="mb-4">
+                The contact form is currently inactive. Get a functional and
+                working contact form with Ajax & PHP in a few minutes. Just copy
+                and paste the files, add a little code and you're done.{" "}
+                <a href="https://freewebsitecreate.net/contact-form">
+                  Download Now
+                </a>
+                .
+              </p>
+              <form>
+                <div className="row g-3">
+                  <div className="col-md-6">
+                    <div className="form-floating">
+                      <input
+                        type="text"
+                        className="form-control"
+                        id="name"
+                        placeholder="Your Name"
+                      ></input>
+                      <label htmlFor="name">Your Name</label>
+                    </div>
+                  </div>
+                  <div className="col-md-6">
+                    <div className="form-floating">
+                      <input
+                        type="email"
+                        className="form-control"
+                        id="email"
+                        placeholder="Your Email"
+                      ></input>
+                      <label htmlFor="email">Your Email</label>
+                    </div>
+                  </div>
+                  <div className="col-12">
+                    <div className="form-floating">
+                      <input
+                        type="text"
+                        className="form-control"
+                        id="subject"
+                        placeholder="Subject"
+                      ></input>
+                      <label htmlFor="subject">Subject</label>
+                    </div>
+                  </div>
+                  <div className="col-12">
+                    <div className="form-floating">
+                      <textarea
+                        className="form-control"
+                        placeholder="Leave a message here"
+                        id="message"
+                        style={{ height: 100 + "px" }}
+                      ></textarea>
+                      <label htmlFor="message">Message</label>
+                    </div>
+                  </div>
+                  <div className="col-12">
+                    <button className="btn btn-primary py-2 px-3 me-3">
+                      Send Message
+                      <div className="d-inline-flex btn-sm-square bg-white text-primary rounded-circle ms-2">
+                        <i className="fa fa-arrow-right"></i>
+                      </div>
+                    </button>
+                  </div>
+                </div>
+              </form>
+            </div>
+            <div
+              className="col-lg-6 wow fadeIn"
+              data-wow-delay="0.5s"
+              style={{ minHeight: 450 + "px" }}
+            >
+              <div className="position-relative rounded overflow-hidden h-100">
+                <iframe
+                  className="position-relative w-100 h-100"
+                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3988.7732487607122!2d36.81825027380594!3d-1.3114253356516767!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x182f11acce585601%3A0x77d09ef85d415926!2sThe%20Zambezi%20Hospital!5e0!3m2!1sen!2ske!4v1706626858464!5m2!1sen!2ske"
+                  // width="600"
+                  // height="450"
+                  loading="lazy"
+                  referrerpolicy="no-referrer-when-downgrade"
+                
+                  //border="0" 
+                  style={{ minHeight: 450 + "px", border: 0 }}
+                  allowFullScreen="" aria-hidden="false" tabIndex="0" >
+                </iframe>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* <!-- Contact End --> */}
     </>
   );
 };
--- src/components/PageHeader.jsx
+++ src/components/PageHeader.jsx
@@ -3,14 +3,14 @@ import React from 'react'
 const PageHeader = () => {
   return (
     // <!-- Page Header Start -->
-    <div class="container-fluid page-header mb-5 wow fadeIn" data-wow-delay="0.1s">
-        <div class="container text-center">
-            <h1 class="display-4 text-white animated slideInDown mb-4">Contact Us</h1>
+    <div className="container-fluid page-header mb-5 wow fadeIn" data-wow-delay="0.1s">
+        <div className="container text-center">
+            <h1 className="display-4 text-white animated slideInDown mb-4">Contact Us</h1>
             <nav aria-label="breadcrumb animated slideInDown">
-                <ol class="breadcrumb justify-content-center mb-0">
-                    <li class="breadcrumb-item"><a class="text-white" href="#">Home</a></li>
-                    <li class="breadcrumb-item"><a class="text-white" href="#">Pages</a></li>
-                    <li class="breadcrumb-item text-primary active" aria-current="page">Contact Us</li>
+                <ol className="breadcrumb justify-content-center mb-0">
+                    <li className="breadcrumb-item"><a className="text-white" href="#">Home</a></li>
+                    <li className="breadcrumb-item"><a className="text-white" href="#">Pages</a></li>
+                    <li className="breadcrumb-item text-primary active" aria-current="page">Contact Us</li>
                 </ol>
             </nav>
         </div>
