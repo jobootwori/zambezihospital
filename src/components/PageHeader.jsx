80d3215d6a467dae279e21974d2cca0ffc283db4
jobootwori <30703963+jobootwori@users.noreply.github.com>
2024-02-01 09:32:15 +0300

    [UPDATE] : Made the pageHeader dynamic

--- src/components/PageHeader.jsx
+++ src/components/PageHeader.jsx
@@ -1,22 +1,40 @@
-import React from 'react'
+import React from "react";
 
-const PageHeader = () => {
+const PageHeader = ({ pageTitle }) => {
   return (
     // <!-- Page Header Start -->
-    <div className="container-fluid page-header mb-5 wow fadeIn" data-wow-delay="0.1s">
-        <div className="container text-center">
-            <h1 className="display-4 text-white animated slideInDown mb-4">Contact Us</h1>
-            <nav aria-label="breadcrumb animated slideInDown">
-                <ol className="breadcrumb justify-content-center mb-0">
-                    <li className="breadcrumb-item"><a className="text-white" href="#">Home</a></li>
-                    <li className="breadcrumb-item"><a className="text-white" href="#">Pages</a></li>
-                    <li className="breadcrumb-item text-primary active" aria-current="page">Contact Us</li>
-                </ol>
-            </nav>
-        </div>
+    <div
+      className="container-fluid page-header mb-5 wow fadeIn"
+      data-wow-delay="0.1s"
+    >
+      <div className="container text-center">
+        <h1 className="display-4 text-white animated slideInDown mb-4">
+          {pageTitle}
+        </h1>
+        <nav aria-label="breadcrumb animated slideInDown">
+          <ol className="breadcrumb justify-content-center mb-0">
+            <li className="breadcrumb-item">
+              <a className="text-white" href="/">
+                Home
+              </a>
+            </li>
+            <li className="breadcrumb-item">
+              <a className="text-white" href="#">
+                Pages
+              </a>
+            </li>
+            <li
+              className="breadcrumb-item text-primary active"
+              aria-current="page"
+            >
+              {pageTitle}
+            </li>
+          </ol>
+        </nav>
+      </div>
     </div>
     // <!-- Page Header End -->
-  )
-}
+  );
+};
 
-export default PageHeader
+export default PageHeader;
