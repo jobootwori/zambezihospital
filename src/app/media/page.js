ed1c782a9f90b5673c047b2fe728306a0725672f
jobootwori <30703963+jobootwori@users.noreply.github.com>
2024-04-25 10:06:07 +0300

    [UPDATE]: Media page update

--- src/app/media/page.js
+++ src/app/media/page.js
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 import React from "react";
 import Header from "../../components/Header";
 import PageHeader from "../../components/PageHeader";
@@ -36,38 +36,27 @@ const page = () => {
     <>
       <Header />
       <PageHeader pageTitle="Media Gallery" />
-      <div className="bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen text-center md:text-left">
-        <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
-          <div className="pb-8">
-            <p className="text-4xl font-bold inline border-b-4 border-gray-500">
-              Title{" "}
-            </p>
-            <p className="py-6">subtitle</p>
-          </div>
 
+      <div className="container-xxl py-5">
+        <div className="container">
           <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 sm:px-5">
             {items.map(({ id, src, link }) => (
-              <div key={id} className="shadow-md shadow-gray-600 rounded-lg overflow-hidden">
-                <Image
-                  src={src}
-                  alt=""
-                  width={400}
-                  height={300}
-                  className="rounded-md duration-200 hover:scale-105"
-                />
-                <div className="flex justify-center mt-4">
-                  <button
-                    className="flex-1 px-6 py-3 m-2 duration-200 hover:scale-105"
-                    onClick={() => window.open(link, "_blank")}
-                  >
-                    Button 1
-                  </button>
-                  <button
-                    className="flex-1 px-6 py-3 m-2 duration-200 hover:scale-105"
-                    onClick={() => window.open(link, "_blank")}
-                  >
-                    Button 2
-                  </button>
+              <div
+                key={id}
+                className="shadow-md shadow-gray-600 rounded-lg overflow-hidden"
+              >
+                <div
+                  className="text-center mx-auto mb-5 wow fadeInUp"
+                  data-wow-delay="0.1s"
+                  style={{ maxWidth: 500 + "px" }}
+                >
+                  <Image
+                    src={src}
+                    alt=""
+                    width={400}
+                    height={300}
+                    className="rounded-md duration-200 hover:scale-105"
+                  />
                 </div>
               </div>
             ))}
