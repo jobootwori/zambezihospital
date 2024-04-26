7d6521708b1a77ae511a383de95ec030e2d7dafe
jobootwori <30703963+jobootwori@users.noreply.github.com>
2024-04-26 10:56:40 +0300

    [UPDATE] : Added Icon

--- src/app/layout.js
+++ src/app/layout.js
@@ -21,6 +21,8 @@ export default function RootLayout({ children }) {
       <head>
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
+        <link rel="icon" href="/img/favicon.ico" sizes="any" />
+
         {/* <!-- Google Web Fonts --> */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
